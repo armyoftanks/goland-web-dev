c28239e0e6ec6158b96e8cd85dd482cb1c6a38f5
Romina Simanovskaya <RSimanovskaya@ipsoft.com>
2018-10-25 04:14:59 -0400

    added some comments

--- 005_data/01/main.go
+++ 005_data/01/main.go
@@ -13,6 +13,7 @@ func init() {
 }
 
 func main() {
+	// only pass in one peice of data agregated data for multiple data (where 42 is)
 	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 42)
 	if err != nil {
 		log.Fatalln(err)
